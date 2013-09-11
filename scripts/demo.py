2d6e431784b3d0fe8661803e03af87296845ce0d
Kazunari Kaizu <kwaizu@gmail.com>
2013-09-11 12:04:48 +0900

    just renamed db4ecellp -> db

--- scripts/demo.py
+++ scripts/demo.py
@@ -5,7 +5,7 @@ import os, sys
 path = os.path.abspath(os.path.dirname(__file__)) + '/..'
 sys.path.append(path)
 
-from db4ecellp import db4ecellp
+from db import db4ecellp
 
 def main():
     _db4ecellp = db4ecellp.Db4ecellp()
