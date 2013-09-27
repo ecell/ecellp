5d4def85037200f2ee11e395fdbcd6655629deff
yukke <soh.ishiguro3914@gmail.com>
2013-09-28 01:38:39 +0900

    Add some unittest suite

--- src/ecellp/tests/query_test.py
+++ src/ecellp/tests/query_test.py
@@ -3,11 +3,12 @@
 import ecellp.session as session
 import unittest
 
+
 class QuerytoDBTest(unittest.TestCase):
     def setUp(self):
         data = '../../../conf.ini'
         self.query = session.QueryBuilder(data)
-
+        
     def tearDown(self):
         print "Done"
         
@@ -17,14 +18,34 @@ class QuerytoDBTest(unittest.TestCase):
         self.assertEqual(rec_count, 4145)
 
     def test_find_by_name(self):
-        gene_name = self.query.find_by_name('thrL').name
-        self.assertEqual(gene_name, 'thrL')
-        #print query.collect_annotations_filter_by_strand(-1)
-        #print query.include_gene_in_region(21, 2012)
+        name = self.query.find_by_name('thrL').name
+        start = self.query.find_by_name('thrL').start
+        end = self.query.find_by_name('thrL').end
+        strand = self.query.find_by_name('thrL').strand
+        sequence = self.query.find_by_name('thrL').sequence
+        feature = self.query.find_by_name('thrL').feature
 
+        self.assertEqual(name, 'thrL')
+        self.assertEqual(start, 189)
+        self.assertEqual(end, 255)
+        self.assertEqual(strand, 1)
+        self.assertEqual(sequence,'ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA')
+        self.assertEqual(feature, 'CDS')
+        
+    def test_collect_annotations_filter_by_strand(self):
+        comp = self.query.collect_annotations_filter_by_strand(-1)
+        dirc = self.query.collect_annotations_filter_by_strand(1)
+        self.assertEqual(len(comp), 2130)
+        self.assertEqual(len(dirc), 2015)
+
+    def test_include_gene_in_region(self):
+        seq = self.query.include_gene_in_region(21, 2012)[0].values()[5]
+        self.assertEqual(seq, "ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA")
+        
     def suite():
         suite = unittest.TestSuite()
         suite.addTest(unittest.makeSuite(QuerytoDBTest))
 
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2).run(suite)
