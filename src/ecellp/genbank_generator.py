b29eed762c3ae81661d89f977abd31b0d265ec11
yukke <soh.ishiguro3914@gmail.com>
2013-09-27 20:25:24 +0900

    Added setup method

--- src/ecellp/genbank_generator.py
+++ src/ecellp/genbank_generator.py
@@ -10,6 +10,12 @@ class GenbankDecGenerator(object):
                       "Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.gbk")
         self.__filename = filename
 
+    def setup(self):
+        if os.path.isfile(self.__filename):
+            return True
+        else:
+            return utils.fetch_url(self.__url, self.__filename)
+
     def generate(self, session):
         with open(self.__filename, "r") as fin:
             for record in SeqIO.parse(fin, 'genbank'):
@@ -26,6 +32,7 @@ class GenbankDecGenerator(object):
 
                             data = (name, strand, start, end, feature, seq)
                             session.add(species.CDSDec(*data))
+                            
                     elif feature.type == 'rRNA':
                         name = feature.qualifiers['gene'][0]
                         start = int(feature.location.start)
@@ -36,6 +43,7 @@ class GenbankDecGenerator(object):
 
                         data = (name, strand, start, end, feature, seq)
                         session.add(species.rRNADec(*data))
+                        
                     elif feature.type == 'tRNA':
                         name = feature.qualifiers['gene'][0]
                         start = int(feature.location.start)
@@ -46,4 +54,6 @@ class GenbankDecGenerator(object):
 
                         data = (name, strand, start, end, feature, seq)
                         session.add(species.tRNADec(*data))
+                        
+        # Insert to DB
         session.commit()
