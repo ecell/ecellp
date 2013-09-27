1b8440904343ef8656f18004de41949a34bdff9b
yukke <soh.ishiguro3914@gmail.com>
2013-09-28 00:24:04 +0900

    add some test

--- src/ecellp/tests/species_test.py
+++ src/ecellp/tests/species_test.py
@@ -10,10 +10,18 @@ class SpeciesSample(unittest.TestCase):
 
     def test_species1(self):
         obj = species.CDSDec("test", +1, 1, 100, "cds", "ATGC")
-
+        
         self.assertEqual(obj.name, "test")
+
+    def test_species2(self):
+        obj = species.CDSDec("test", +1, 1, 100, "cds", "ATGC")
+        self.assertEqual(obj.feature, "cds")
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTests(unittest.makeSuite(SpeciesSample))
     return suite
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
